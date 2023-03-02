2b3d104c704ddf4bfeac3d64ee4066fac59324f5
admin <g.lyalko@mail.ru>
2023-03-02 19:00:41 +0300

    3) Cохранения состояния при повороте экрана

--- app/src/main/java/com/hfad/stopwatch/MainActivity.kt
+++ app/src/main/java/com/hfad/stopwatch/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hfad.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.os.SystemClock
 import android.widget.Button
 import android.widget.Chronometer
@@ -16,11 +17,28 @@ class MainActivity : AppCompatActivity() {
     lateinit var stopwatch: Chronometer
     var running = false
     var offset: Long = 0
+
+    /*добавляем ключи (key) типа String для работы c Bundle.
+     Используем три константы в качестве имен для любых значений, которые мы добавим в Bundle*/
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         stopwatch = findViewById<Chronometer>(R.id.stopwatch)//получаем ссылку на Chronometer
+
+        /**Восстанавливаем предыдущее состояние*/
+        if (savedInstanceState != null){
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (running){
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            }else setBaseTime()
+        }
+
 
         //кнопка startButton запускакет секундомер, если он не запущен
         val startButton = findViewById<Button>(R.id.start_button)
@@ -31,28 +49,40 @@ class MainActivity : AppCompatActivity() {
                 running = true
             }
         }
-//кнопка pauseButton ставит на паузу секундомер, есди он запущен
+        //кнопка pauseButton ставит на паузу секундомер, есди он запущен
         var pauseButton = findViewById<Button>(R.id.button_pause)
         pauseButton.setOnClickListener {
-            if (running){
+            if (running) {
                 saveOffset()//сохраняет время на секундомере
                 stopwatch.stop()
                 running = false
             }
         }
         //кнопка resetButton устанавливает Свойство offset = 0
-        var resetButton= findViewById<Button>(R.id.button_reset)
+        var resetButton = findViewById<Button>(R.id.button_reset)
         resetButton.setOnClickListener {
             offset = 0
             setBaseTime()//устанавливаем секундомер назад на 0
         }
     }
-    //обновляем stopwatch.base, учитывая любое смещение (offset)
-    fun setBaseTime (){
-        stopwatch.base = SystemClock.elapsedRealtime()-offset
+
+    /**  Сохраняем состояние базовых свойств*/
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putLong(OFFSET_KEY, offset)
+        outState.putBoolean(RUNNING_KEY, running)
+        outState.putLong(BASE_KEY, stopwatch.base)
+        super.onSaveInstanceState(outState)
     }
+
+    //обновляем stopwatch.base, учитывая любое смещение (offset)
+    fun setBaseTime() {
+        stopwatch.base = SystemClock.elapsedRealtime() - offset
+    }
+
     //записываем смещение
-    fun saveOffset (){
+    fun saveOffset() {
         offset = SystemClock.elapsedRealtime() - stopwatch.base
     }
+
+
 }
