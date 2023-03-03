f2d1560ce714a1613121436839a2de26b8b8a3bd
admin <g.lyalko@mail.ru>
2023-03-03 18:12:09 +0300

    5) методы onStop(), onRestart() заменены на onPause() и onResume

--- app/src/main/java/com/hfad/stopwatch/MainActivity.kt
+++ app/src/main/java/com/hfad/stopwatch/MainActivity.kt
@@ -73,16 +73,16 @@ class MainActivity : AppCompatActivity() {
         super.onSaveInstanceState(outState)
     }
 
-    override fun onStop() {
-        super.onStop()
+    override fun onPause() {
+        super.onPause()
         if (running){
             saveOffset()
             stopwatch.stop()
         }
     }
 
-    override fun onRestart() {
-        super.onRestart()
+    override fun onResume() {
+        super.onResume()
         if (running){
             setBaseTime()
             stopwatch.start()
